8f10ff21c9ed36093f5ddf898aeafb4dbdc82eab
MrCosik <s20144@pjwstk.edu.pl>
2020-08-20 12:32:39 +0200

    Added two methods to transform 1D array to 2D and print it out in the console

--- src/pl/pjatk/edu/Main/Main.java
+++ src/pl/pjatk/edu/Main/Main.java
@@ -1,10 +1,41 @@
 package pl.pjatk.edu.Main;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
-        System.out.println("X O X");
-        System.out.println("O X O");
-        System.out.println("X X O");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Hello, enter 9 characters:");
+
+
+        String userInput = scan.nextLine();
+        char[] allCharacters = userInput.toCharArray();
+        char[][] sortedChars = new char[3][3];
+
+        System.out.println("---------");
+        transform1Dto2DArray(allCharacters, sortedChars);
+
+        Show2DArray(sortedChars);
+
+        System.out.println("---------");
+    }
+
+    private static void Show2DArray(char[][] sortedChars) {
+        for(int i = 0; i < 3; i++){
+            System.out.print("| ");
+            for(int j = 0; j < 3; j++){
+                System.out.print(sortedChars[i][j] + " ");
+            }
+            System.out.print(" |");
+            System.out.println();
+        }
+    }
+
+    private static void transform1Dto2DArray(char[] allCharacters, char[][] sortedChars) {
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                sortedChars[i][j] = allCharacters[j%3+i*3];
+            }
+        }
     }
 }
